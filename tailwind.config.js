1400c9843740440b91d15340ca0f06c7deb93dc5
MLxpezz <mlopez_098@hotmail.com>
2024-07-12 11:02:55 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary-color": "#3FB3F1",
+        "blue-strong": "#0A2D47",
+        "blue-white": "#E1F1FD",
+        "blue-medio": "#3FB3F1",
+        "blue-medio-strong": "#097BC0"
+      }
+    },
+  },
+  plugins: [],
+}
+
