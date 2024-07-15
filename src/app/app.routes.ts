891c64e2532b907cfa8052e9d28afb4ce348964c
MLxpezz <mlopez_098@hotmail.com>
2024-07-15 12:31:21 -0600

    added header for dashboard view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "", redirectTo: "/login", pathMatch: 'full'
+    },
+    {
+        path: "",  loadChildren: () => import("./auth/auth.route").then(routes => routes.AUTH_ROUTES)
+    },
+    {
+        path: "dashboard", loadChildren: () => import("./admin/admin.routes").then(routes => routes.ADMIN_ROUTES)
+    }
+];
