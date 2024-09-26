641bce926a593fddbc90a8c4d88589f8c71e00a2
MLxpezz <mlopez_098@hotmail.com>
2024-09-26 10:08:59 -0600

    add table to account component

--- src/app/admin/pages/account/account.component.ts
+++ src/app/admin/pages/account/account.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FadeDirective } from '../../../components/directives/fade/fade.directive';
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FadeDirective],
   templateUrl: './account.component.html',
-  styleUrl: './account.component.css'
+  styleUrl: './account.component.css',
 })
 export class AccountComponent {
+  canEditInfo: boolean = false;
+  textButton: string = 'Editar mi informacion';
 
+  editInfo() {
+    this.canEditInfo = !this.canEditInfo;
+
+    if (this.canEditInfo) {
+      this.textButton = 'Cancelar';
+    } else {
+      this.textButton = 'Editar mi informacion';
+    }
+  }
 }
